d9a610abfe851ac1695714ca4a7b4ed46223e179
Andrey Rastegaev <embrion83@yandex.ru>
2021-09-12 21:35:05 +0300

    add strapi config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -35,7 +35,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/strapi'
   ],
+
+  strapi: {
+    entities: ['profiles'],
+    url: 'http://localhost:1337'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
